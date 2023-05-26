4842de7d2e64222ffcc354972a75870eb4e3237b
Megha Dandapat <megha13dandapat@gmail.com>
2023-05-27 02:56:12 +0530

    Update Bully.java

--- Assign6/Bully.java
+++ Assign6/Bully.java
@@ -1,88 +1,116 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class BullyAlgorithm {
-    static int[] state = new int[5];
-    static int coordinator;
+public class Bully {
+    int coordinator;
+    int max_processes;
+    boolean processes[];
 
-    public static void up(int up) {
-        if (state[up - 1] == 1) {
-            System.out.println("Process " + up + " is already up.");
+
+    public Bully(int max) {
+        max_processes = max;
+        processes = new boolean[max_processes];
+        coordinator = max;
+
+        System.out.println("Creating processes..");
+        for(int i = 0; i < max; i++) {
+            processes[i] = true;
+            System.out.println("P"+ (i+1) + " created");
+        }
+        System.out.println("Process P" + coordinator + " is the coordinator");
+
+    }
+    void displayProcesses() {
+        for(int i = 0; i < max_processes; i++) {
+            if(processes[i]) {
+                System.out.println("P" + (i+1) + " is up");
+            } else {
+                System.out.println("P" + (i+1) + " is down");
+            }
+        }
+        System.out.println("Process P" + coordinator + " is the coordinator");
+    }
+
+    void upProcess(int process_id) {
+        if(!processes[process_id - 1]) {
+            processes[process_id - 1] = true;
+            System.out.println("Process " + process_id + " is now up.");
         } else {
-            state[up - 1] = 1;
-            System.out.println("Process " + up + " initiated an election.");
-
-            // Send election messages to higher-numbered processes
-            for (int i = up + 1; i <= 5; i++) {
-                if (state[i - 1] == 1) {
-                    sendMessage(up, i, "ELECTION");
-                }
-            }
-
-            // Receive alive messages from lower-numbered processes
-            for (int i = up - 1; i >= 1; i--) {
-                if (state[i - 1] == 1) {
-                    System.out.println("Alive message received from process " + i + " by process " + up);
-                    break;
-                }
-            }
-
-            // Set the coordinator to the highest-numbered process that responded
-            coordinator = up;
-            System.out.println("Process " + coordinator + " is the coordinator.");
+            System.out.println("Process " + process_id + " is already up.");
         }
     }
 
-    public static void down(int down) {
-        if (state[down - 1] == 0) {
-            System.out.println("Process " + down + " is already down.");
+    void downProcess(int process_id) {
+        if(!processes[process_id - 1]) {
+            System.out.println("Process " + process_id + " is already down.");
         } else {
-            state[down - 1] = 0;
-            System.out.println("Process " + down + " is down.");
+            processes[process_id - 1] = false;
+            System.out.println("Process " + process_id + " is down.");
+        }
+    }
 
-            // If the coordinator process is down, start a new election
-            if (coordinator == down) {
-                for (int i = 1; i <= 5; i++) {
-                    if (state[i - 1] == 1) {
-                        up(i);
-                        break;
-                    }
-                }
+    void runElection(int process_id) {
+        coordinator = process_id;
+        boolean keepGoing = true;
+
+        for(int i = process_id; i < max_processes && keepGoing; i++) {
+            System.out.println("Election message sent from process " + process_id + " to process " + (i+1));
+
+            if(processes[i]) {
+                keepGoing = false;
+                runElection(i + 1);
             }
         }
     }
 
-    public static void sendMessage(int sender, int receiver, String message) {
-        System.out.println("Message sent from process " + sender + " to process " + receiver + ": " + message);
-    }
-
-    public static void main(String[] args) {
+    public static void main(String args[]) {
+        Bully bully = null;
+        int max_processes = 0, process_id = 0;
+        int choice = 0;
         Scanner sc = new Scanner(System.in);
-        int choice;
-        coordinator = 5;
 
-        do {
-            System.out.println("---------");
-            System.out.println("1. Up a process.");
-            System.out.println("2. Down a process.");
-            System.out.println("3. Exit.");
-            System.out.println("---------");
-            System.out.println("Enter your choice: ");
+        while(true) {
+            System.out.println("Bully Algorithm");
+            System.out.println("1. Create processes");
+            System.out.println("2. Display processes");
+            System.out.println("3. Up a process");
+            System.out.println("4. Down a process");
+            System.out.println("5. Run election algorithm");
+            System.out.println("6. Exit Program");
+            System.out.print("Enter your choice:- ");
             choice = sc.nextInt();
 
-            switch (choice) {
-                case 1:
-                    System.out.println("Bring up a process: ");
-                    int up = sc.nextInt();
-                    up(up);
+            switch(choice) {
+                case 1: 
+                    System.out.print("Enter the number of processes:- ");
+                    max_processes = sc.nextInt();
+                    bully = new Bully(max_processes);
                     break;
                 case 2:
-                    System.out.println("Bring down a process: ");
-                    int down = sc.nextInt();
-                    down(down);
+                    bully.displayProcesses();
+                    break;
+                case 3:
+                    System.out.print("Enter the process number to up:- ");
+                    process_id = sc.nextInt();
+                    bully.upProcess(process_id);
+                    break;
+                case 4:
+                    System.out.print("Enter the process number to down:- ");
+                    process_id = sc.nextInt();
+                    bully.downProcess(process_id);
+                    break;
+                case 5:
+                    System.out.print("Enter the process number which will perform election:- ");
+                    process_id = sc.nextInt();
+                    bully.runElection(process_id);
+                    bully.displayProcesses();
+                    break;
+                case 6:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Error in choice. Please try again.");
                     break;
             }
-        } while (choice != 3);
+        }
     }
 }
-
-
