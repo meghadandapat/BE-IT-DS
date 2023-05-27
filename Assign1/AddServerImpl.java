8d9b4142fd28aa9ed546af95e30feeda484d6f2a
Megha Dandapat <megha13dandapat@gmail.com>
2023-05-28 03:23:31 +0530

    Update AddServerImpl.java

--- Assign1/AddServerImpl.java
+++ Assign1/AddServerImpl.java
@@ -1,9 +1,12 @@
 import java.rmi.*;
 import java.rmi.server.*;
+//class that implements the remote interface
 public class AddServerImpl extends UnicastRemoteObject 
 implements AddServerIntf {
+//constructor
 public AddServerImpl() throws RemoteException {
 }
+//implement method declared in the interface
 public double add(double d1, double d2) throws RemoteException {
  return d1 + d2; }
 }
