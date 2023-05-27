c79eb73ffaae7fe43465c336bbc477319dde0cb5
Megha Dandapat <megha13dandapat@gmail.com>
2023-05-28 03:28:38 +0530

    Update AddServer.java

--- Assign1/AddServer.java
+++ Assign1/AddServer.java
@@ -4,7 +4,9 @@ import java.rmi.*;
 public class AddServer {
 public static void main(String args[]) {
 try { 
+//create remote object
 AddServerImpl addServerImpl = new AddServerImpl(); 
+//bind the remote object
 Naming.rebind("AddServer", addServerImpl);
 }
 catch (Exception e) {
