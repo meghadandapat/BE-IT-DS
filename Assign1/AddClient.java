4b43ba743c897742c6c9110ce9662ffed7a3709c
Megha Dandapat <megha13dandapat@gmail.com>
2023-05-28 03:35:26 +0530

    Update AddClient.java

--- Assign1/AddClient.java
+++ Assign1/AddClient.java
@@ -2,13 +2,15 @@ import java.rmi.*;
 public class AddClient {
 public static void main(String args[]) { 
 try {
+// Get reference to the remote object
 String addServerURL = "rmi://" + args[0] + "/AddServer"; 
 AddServerIntf addServerIntf =
 (AddServerIntf) Naming.lookup(addServerURL);
 System.out.println("The first number is: " + args[1]); 
-double d1 = Double.valueOf(args[1]).doubleValue();
+double d1 = Double.parseDouble(args[1]);
 System.out.println("The second number is: " + args[2]);
-double d2 = Double.valueOf(args[2]).doubleValue();
+double d2 = Double.parseDouble(args[2]);
+// Invoke remote method to add numbers
 System.out.println("The sum is: " + addServerIntf.add(d1, d2));
 } 
 catch (Exception e) { System.out.println("Exception: "+ e);
